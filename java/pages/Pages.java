3faf83758ee080f01ba8ee292dbfd04640fb3e5c
Durairaji <112107468+Durairaji@users.noreply.github.com>
2023-01-07 01:28:18 +0530

    facebook automate

--- java/pages/Pages.java
+++ java/pages/Pages.java
@@ -1,5 +1,50 @@
 package pages;
 
-public class Pages {
+import org.openqa.selenium.WebElement;
 
+import seleniumBase.Locators;
+import seleniumBase.SeleniumBase;
+
+public class Pages extends SeleniumBase {
+	
+	public boolean loginLable(String value) {
+		
+		WebElement element = element(Locators.xpath, value);
+		return element.isEnabled();
+		
+	
+	}
+	public boolean forgetLable(String value)
+	{
+		return element(Locators.link,value).isEnabled();
+	}
+	
+	public boolean newAccoundLable(String value) {
+		return element(Locators.link, value).isEnabled();
+	}
+	
+	public boolean iyeLable(String value) {
+		return element(Locators.xpath, value).isDisplayed();
+	}
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
 }
+
+
+
+
+//
+//public  boolean usernameLable()
+//{
+//	WebElement element = element(Locators.xpath, "(//td[@align='right'])[1]");
+//return	element.isEnabled();
+//}
--- java/testData/FB_TC001.java
+++ java/testData/FB_TC001.java
@@ -1,8 +1,11 @@
 package testData;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import pages.Pages;
+import seleniumBase.Locators;
 import startBase.StartingBase;
 
 public class FB_TC001 extends StartingBase 
@@ -13,11 +16,20 @@ public class FB_TC001 extends StartingBase
 		fileName = "TC001";
 		url="https://www.facebook.com/";
 		
+		
 	}
-	@Test
-	public void loginTest() {
+	
+	@Test(dataProvider="data")
+	public void loginTest(String data[]) {
 		
+		Pages pg=new Pages();
+		Assert.assertEquals(pg.loginLable("//button[@type='submit']"), true);
+		Assert.assertEquals(pg.forgetLable("Forgotten password?"), true);
+		Assert.assertEquals(pg.newAccoundLable("Create New Account"), true);
 		
+		type(element(Locators.id, "email"),data[0]);
+		type(element(Locators.id,"pass"),data[1]);
+	
 		
 	}
 	
@@ -36,3 +48,7 @@ public class FB_TC001 extends StartingBase
 	
 	
 }
+//Assert.assertEquals(pg.iyeLable("//div[@class='_9lsb _9ls8']"), true);
+//Assert.assertEquals(pg.iyeLable("//div[@class='_9lsb _9ls8']"), true);
+//boolean loginLable = pg.loginLable("//button[@type='submit']");
+
