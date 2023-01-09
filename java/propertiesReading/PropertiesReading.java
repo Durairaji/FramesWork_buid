30384d38379b8634b59205896464addde92f1e5e
Durairaji <112107468+Durairaji@users.noreply.github.com>
2023-01-10 01:15:45 +0530

    sample report

--- java/propertiesReading/PropertiesReading.java
+++ java/propertiesReading/PropertiesReading.java
@@ -5,14 +5,26 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class PropertiesReading {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		Properties properties = new Properties();
 		properties.load(new FileInputStream("C:\\Users\\DURAIRAJ\\eclipse-workspace\\MavanFrame\\src\\test\\java\\propertiesReading\\Login.properties"));
-		String property = properties.getProperty("password");
-		System.out.println(property);
+		//String property = properties.getProperty("password");
+		//String property2 = properties.getProperty("username");
+//		System.out.println(property);
+//		System.out.println(property2);
+//	
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
 		
+		driver.findElement(By.id("email")).sendKeys(properties.getProperty("username"));
+		driver.findElement(By.id("pass")).sendKeys(properties.getProperty("username"));
 		
 		
 		
